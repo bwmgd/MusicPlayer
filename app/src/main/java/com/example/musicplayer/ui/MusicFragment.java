a6299c7ace5b603057d09be5dee32dbef8fee42d
bwmgd <1971138851@qq.com>
2020-11-15 01:55:15 +0800

    修复了拖动音乐改变顺序闪退的bug,并使列表顺序可以保存

--- app/src/main/java/com/example/musicplayer/ui/MusicFragment.java
+++ app/src/main/java/com/example/musicplayer/ui/MusicFragment.java
@@ -56,7 +56,7 @@ public class MusicFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.music_fragment_item_list, container, false);
+        View view = inflater.inflate(R.layout.music_list_fragment, container, false);
 
         // Set the adapter
         if (view instanceof RecyclerView) {
